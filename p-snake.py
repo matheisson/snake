3eeb826f6489e672bca46988c8eee26b3a5722cb
cickib <bellakocsis06@gmail.com>
2016-05-31 13:31:53 +0200

    colors

--- p-snake.py
+++ p-snake.py
@@ -1,58 +1,62 @@
 import curses
+from curses import wrapper
+from curses import KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
 import time
 import random
-import sys
 
 screen = curses.initscr()
-title = ' !WELCOME TO PYTHON! '
-screen.keypad(1)  # Enable keypad
 dims = screen.getmaxyx()
-# with this variable we can teg the max Y,X coords,
-# equal to COLS-LEN
+title = " 💩 CuRsEsSnAkE 💩  "
+start_length = 5
+growby = 1
+speed = {"Easy": 0.2, "Medium": 0.09, "Hard": 0.05}
+difficulty = "Medium"
+accel = True
 
 
-def game():
-    # Setting the screen
-    curses.noecho()
-    curses.curs_set(0)
+def game(screen):
+    screen.clear()
+    dims = screen.getmaxyx()
+    curses.use_default_colors()
     screen.nodelay(1)
-    head = [1, 1, ]  # Snake starts from the top left corner
-    body = [head[:]] * 5  # Copy the head
+    curses.curs_set(0)
+    screen.keypad(1)
+    head = [1, 1]
+    body = [head[:]] * start_length
     screen.border()
-    direction = 0  # default direction = right
 
-    curses.start_color()
-
-    game_over = False  # by setting it false we can star a while Loop later
-    screen.addstr(0, (curses.COLS - len(title)) // 2, title)
-    deadcell = body[-1][:]
+    screen.addstr(0, (dims[1] - len(title)) // 2, title)
+    nam = "© cickib"
+    screen.addstr(dims[0] - 1, dims[1] - 2 * len(nam), nam)
+    direction = 0  # 0-right, 1-down, 2-left, 3, up
+    game_over = False
     foodmade = False
-    body_part = "█"
-
+    deadcell = body[-1][:]
+    key = 0
     while not game_over:
-        score = ("SCORE: " + str(len(body) - 5))
-        screen.addstr(0, (curses.COLS - len(score)) // 7, score)
-        while not foodmade:  # Generating food to random places on the screen
+        while not foodmade:
             y, x = random.randrange(
                 1, dims[0] - 1), random.randrange(1, dims[1] - 1)
-            if screen.inch(y, x) == ord(" "):  # within the border
+            if screen.inch(y, x) == ord(" "):
                 foodmade = True
-                screen.addch(y, x, ord('@'))
+#                g = random.randint(1, 3)
+                screen.addch(y, x, ord("#"))
         if deadcell not in body:
             screen.addch(deadcell[0], deadcell[1], " ")
-        screen.addch(head[0], head[1], body_part)
-
-        # Geeting key inputs, and pairing them with 4 directions
+# screen.addch(head[0], head[1], "x", curses.color_pair(g))
+        screen.addch(head[0], head[1], "x")
         action = screen.getch()
         if action == curses.KEY_UP and direction != 1:
             direction = 3
-        elif action == curses.KEY_DOWN and direction != 3:
+        if action == curses.KEY_DOWN and direction != 3:
             direction = 1
-        elif action == curses.KEY_RIGHT and direction != 2:
-            direction = 0
-        elif action == curses.KEY_LEFT and direction != 0:
+        if action == curses.KEY_LEFT and direction != 0:
             direction = 2
-        # Giving directions, telling the head where to move on the Y,X coords
+        if action == curses.KEY_RIGHT and direction != 2:
+            direction = 0
+        if action == 27:
+            # exit()
+            break
         if direction == 0:
             head[1] += 1
         elif direction == 2:
@@ -65,89 +69,155 @@ def game():
         deadcell = body[-1][:]
         for z in range(len(body) - 1, 0, -1):
             body[z] = body[z - 1][:]
-
         body[0] = head[:]
-        # defing game over and growth of the snake
-        if screen.inch(head[0], head[1]) != ord(' '):
-            if screen.inch(head[0], head[1]) == ord('@'):  # eating food, making the snake bigger
+
+        if head[0] == dims[0] - 1 and head[1] == dims[1] - len(body):
+            if direction == 0:
+                y -= 1
+            elif direction == 1:
+                x -= 1
+
+    # addig megy, amíg olyasmibe nem ütközik, ami nem space => fal
+        if screen.inch(head[0], head[1]) != ord(" "):
+            if screen.inch(head[0], head[1]) == ord("#"):
                 foodmade = False
-                body.append(body[-1])
-            elif screen.inch(head[0], head[1]) == body_part:
-                game_over = True
+                for g in range(growby):
+                    body.append(body[-1])
             else:
                 game_over = True
-
-        screen.move(dims[0] - 1, dims[1] - 1)
         screen.refresh()
-        time.sleep(0.1)
-        if len(body) <= 7:  # Here we added acceleratin to the game, based on the snakes length
-            time.sleep(0.07)
-        elif len(body) < 10:
-            time.sleep(0.05)
-        elif len(body) < 15:
-            time.sleep(0.04)
-        elif len(body) < 22:
-            time.sleep(0.02)
-        elif len(body) < 31:
-            time.sleep(0.01)
-        win = ("You have won the game. You don't have a life")  # :-)
-        if len(body) == (curses.COLS * curses.LINES):
-            game_over = True
+        if not accel:
+            time.sleep(speed[difficulty])
+        else:
+            time.sleep(15.0 * speed[difficulty] / len(body))
     screen.clear()
+
     screen.nodelay(0)
-    score = str(len(body) - 5)
-    message = "GAME OVER"
-    message2 = "You Got " + score + " Points"
-    message3 = "Press SPACE to Play Again"
-    message4 = "Press ENTER to Quit"
-    if len(body) == (curses.COLS * curses.LINES):
-        screen.addstr(6, (curses.COLS - len(win)) // 2, win)
-    else:
-        screen.addstr(6, (curses.COLS - len(message)) // 2, message)
-    screen.addstr(7, (curses.COLS - len(message2)) // 2, message2)
-    screen.addstr(8, (curses.COLS - len(message3)) // 2, message3)
-    screen.addstr(9, (curses.COLS - len(message4)) // 2, message4)
+    message1 = "Game Over Fucker!"
+    message2 = "You got " + \
+        str((len(body) - start_length) // growby) + " points."
+    message3 = "Press Space to play again."
+    message4 = "Press Esc to quit."
+    message5 = "Press M to go back to the menu."
+    screen.addstr(dims[0] // 2 - 3, (dims[1] - len(message1)) // 2, message1)
+    screen.addstr(dims[0] // 2 - 2, (dims[1] - len(message2)) // 2, message2)
+    screen.addstr(dims[0] // 2, (dims[1] - len(message3)) // 2, message3)
+    screen.addstr(dims[0] // 2 + 1, (dims[1] - len(message4)) // 2, message4)
+    screen.addstr(dims[0] // 2 + 2, (dims[1] - len(message5)) // 2, message5)
     screen.refresh()
-    k = 0  # here we can exit or play again by pressing the right key
-    while k not in [32, 10]:
-        k = screen.getch()
-    if k == 32:
-        screen.clear()
-        game()
-    if k == 10:
-        screen.clear()
+    q = 0
+    while q not in [32, 27, 77, 109]:
+        q = screen.getch()
+    if q == 32:
+        game(screen)
+    elif q == 27:
         exit()
+    elif q == 77 or q == 109:
+        menu(screen)
 
 
-def menu(stdscr):
-    screen.nodelay(0)
-    curses.noecho()
+def menu(screen):
     curses.curs_set(0)
+    screen.nodelay(0)
+    curses.use_default_colors()
+    screen.clear()
     selection = -1
     option = 0
-    while selection < 0:  # change it by sel. in menu and hitting Enter
+    while selection < 0:
+	# This line means graphics will first look like [curses.A_REVERSE, 0, 0, 0, 0]
+	# and the option number will determine where the attribute goes.
         graphics = [0] * 5
         graphics[option] = curses.A_REVERSE
-        screen.addstr(0, dims[1] // 2 - 7, "WELCOME TO PYTHON")
-        screen.addstr(6, dims[1] // 2 - 2, "PLAY", graphics[0])
-        screen.addstr(7, dims[1] // 2 - 2, "EXIT", graphics[1])
+        screen.addstr(dims[0] // 2 - 2, dims[1] // 2 - 2, "Play", graphics[0])
+        screen.addstr(dims[0] // 2 - 1, dims[1] // 2 - 2, "Info", graphics[1])
+        screen.addstr(
+            dims[0] // 2, dims[1] // 2 - 6, "Game Options", graphics[2])
+        screen.addstr(dims[0] // 2 + 1, dims[
+                      1] // 2 - 5, "High Score", graphics[3])
+        screen.addstr(dims[0] // 2 + 2, dims[1] // 2 - 2, "Exit", graphics[4])
+        screen.refresh()
+        action = screen.getch()
+        if action == curses.KEY_UP:
+            option = (option - 1) % 5
+        elif action == curses.KEY_DOWN:
+                option = (option + 1) % 5
+        elif action == ord("\n"):
+            selection = option
+    if selection == 0:
+        game(screen)
+    elif selection == 1:
+        info(screen)
+    elif selection == 2:
+        gameOptions(screen)
+
+
+def info(screen):
+    screen.clear()
+    screen.nodelay(0)
+    infos_top = [" 💩  CuRsEsSnAkE 💩  "]
+    infos_center = [
+        "Use the arrow keys to move.", "Don't run into the wall, title, or yourself.",
+        "Collect food to grow.", "\n", "And remember, that the snake gets longer as well as FASTER."]
+    infos_bottom = ["Press any key to go back."]
+    for t in range(len(infos_top)):
+        screen.addstr(t + 1, (dims[1] - len(infos_top[t])) // 2, infos_top[t])
+    for z in range(len(infos_center)):
+        screen.addstr((dims[0] - len(infos_center)) // 2 + z, (
+            dims[1] - len(infos_center[z])) // 2, infos_center[z])
+    for b in range(len(infos_bottom)):
+        screen.addstr(dims[0] + b - 1 - len(infos_bottom), (
+            dims[1] - len(infos_bottom[b])) // 2, infos_bottom[b])
+    screen.refresh()
+    screen.getch()
+    menu(screen)
+
+
+def gameOptions(screen):
+    global start_length, growby, difficulty, accel
+    screen.clear()
+    selection = -1
+    option = 0
+    while selection < 4:
+        screen.clear()
+        graphics = [0] * 5
+        graphics[option] = curses.A_BOLD
+        strings = ["Starting snake length? " + str(start_length), "Snake growth rate: " +
+                   str(growby), "Difficulty: " +
+                   str(difficulty), "Acceleration: " + str(accel),
+                   "Exit"]
+        for z in range(len(strings)):
+            screen.addstr((dims[0] - len(strings)) // 2 + z, (
+                dims[1] - len(strings[z])) // 2, strings[z], graphics[z])
         screen.refresh()
         action = screen.getch()
         if action == curses.KEY_UP:
             option = (option - 1) % 5
         elif action == curses.KEY_DOWN:
             option = (option + 1) % 5
-        elif action == ord('\n'):
+        elif action == ord("\n"):
             selection = option
-    screen.clear()
-    if selection == 0:
-        game()
-
-    elif selection == 1:
-        screen.clear()
-        screen.refresh()
-        exit()
-
-curses.wrapper(menu)
-
-curses.endwin()
+        elif action == curses.KEY_RIGHT and option in [0, 1]:
+            if option == 0 and start_length < 20:
+                start_length += 1
+            elif option == 1 and growby < 10:
+                growby += 1
+        elif action == curses.KEY_LEFT:
+            if option == 0 and start_length > 3:
+                start_length -= 1
+            elif option == 1 and growby > 1:
+                growby -= 1
+        elif action == 27:
+            menu(screen)
+        if selection == 2:
+            if difficulty == "Easy":
+                difficulty = "Medium"
+            elif difficulty == "Medium":
+                difficulty = "Hard"
+            else:
+                difficulty = "Easy"
+        elif selection == 3:
+            accel = not accel
+        if selection < 4:
+            selection = -1
+    menu(screen)
+wrapper(menu)
